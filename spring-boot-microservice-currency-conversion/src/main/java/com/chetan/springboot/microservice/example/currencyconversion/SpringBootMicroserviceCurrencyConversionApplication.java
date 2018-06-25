17e783745140e63bc0638cefbc75e8feb8ff771a
chetan <bairagi.sirfire@gmail.com>
2018-06-25 22:49:26 +0530

    This project contains a service provider and consumer via Rest Template.In next commits we will put feighn,ribbon and eureka to work.Read Readme for More

---
+++ spring-boot-microservice-currency-conversion/src/main/java/com/chetan/springboot/microservice/example/currencyconversion/SpringBootMicroserviceCurrencyConversionApplication.java
@@ -0,0 +1,11 @@
+package com.chetan.springboot.microservice.example.currencyconversion;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroserviceCurrencyConversionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroserviceCurrencyConversionApplication.class, args);
+	}
+}
